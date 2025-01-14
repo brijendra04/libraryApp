341b981094f253e95bb1652cfb4232e03b9f7610
Brijendra Pratap <brijendraoffice@gmail.com>
2025-01-14 19:39:58 +0530

    test

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,33 @@
-import React from 'react'
-import Home from '../src/Home/Home'
-import Courses from './courses/Courses'
-import {Navigate, Route, Routes} from 'react-router-dom'
-import Signup from './components/Signup'
-import Contact from '../src/contact/Contact'
-import toast from 'react-hot-toast'
-import { Toaster } from 'react-hot-toast'
-import About from './about/About'
-import { useAuth } from './context/AuthProvider'
-
+import React from "react";
+import Home from "../src/Home/Home";
+import Courses from "./courses/Courses";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Signup from "./components/Signup";
+import Contact from "../src/contact/Contact";
+import { Toaster } from "react-hot-toast";
+import About from "./about/About";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
   const [authUser, setAuthUser] = useAuth();
-  console.log(authUser)
+  console.log("User", authUser);
   return (
     <>
-    <div className="dark:bg-slate-900 dark:text-white">
-    <Routes>
-      <Route path="/" element={<Home/>}/>
-      <Route path="/course" element={authUser ? <Courses/>: <Navigate to="/signup"/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-      <Route path="/about" element={<About/>}/>
-      
-      
-    </Routes>
-    <Toaster/>
-    </div>
-    
+      <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/courses"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/contacts" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Toaster />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
