073592fc60ff084492163bfd062a732668279c44
Brijendra Pratap <brijendraoffice@gmail.com>
2025-01-14 18:44:21 +0530

    mongo atlas  env

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,19 @@ import cors from 'cors';
 import bookRoute from './route/book.route.js'
 import userRoute from './route/user.route.js'
 
-const app = express();
-app.use(cors());
+const app = express()
+
+app.use(cors(
+    {
+        origin: ["https://libraryapp-eosin.vercel.app/"],
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+
+));
 app.use(express.json());
+
+
 
 dotenv.config();
 const PORT = process.env.PORT || 4000;
